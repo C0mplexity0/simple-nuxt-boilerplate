f6cd67392555f4e5113de758f1becb007ebdb305
C0mplexity0 <106925362+C0mplexity0@users.noreply.github.com>
2026-01-17 18:59:25 +0000

    feat: Page structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/a11y',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image'
-  ]
-})
+  modules: ["@nuxt/a11y", "@nuxt/eslint", "@nuxt/fonts", "@nuxt/image"],
+
+  app: {
+    head: {
+      title: "Simple Nuxt Boilerplate",
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+});
